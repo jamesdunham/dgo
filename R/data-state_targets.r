abb8850a40223d2b6a505d03148b53f8df58b75b
James Dunham <james@jamesdunham.io>
2015-12-02 19:37:30 -0500

    Add state targets data

---
+++ R/data-state_targets.r
@@ -0,0 +1,21 @@
+#' State-level demographic marginals
+#'
+#' A dataset with the proportion of each state's population 1960-2010 in strata defiend by four demograhic variables. 
+#'
+#' \enumerate{
+#'  \item state
+#'  \item year
+#'  \item race
+#'  \item female
+#'  \item education
+#'  \item age
+#' }
+#'
+#' @docType data
+#' @name state_targets
+#' @usage state_targets
+#' @format A \code{dplyr} \code{tbl_df} with 312,120 rows and 7 columns.
+#' @examples
+#' state_targets
+NULL
+ 
