9441955bce9f2311410a0a8509fa1ef0db1bb8e4
James Dunham <james@jamesdunham.io>
2015-12-10 09:21:25 -0500

    Remove stringr dependency

--- tests/testthat/test-dgirt.r
+++ tests/testthat/test-dgirt.r
@@ -12,16 +12,16 @@ expect_error(dump_dgirt(data.frame()))
 context("Get dgirt executable path")
 
 expect_match(get_dgirt_path(), "dgirt$")
-expect_true(stringr::str_length(get_dgirt_path()) > 0)
+expect_true(stringi::stri_length(get_dgirt_path()) > 0)
 
 context("Get cmdstan output path")
 
 expect_match(get_output_path(), "/output.csv$")
-expect_true(stringr::str_length(get_output_path()) >
-  stringr::str_length("/output.csv"))
+expect_true(stringi::stri_length(get_output_path()) >
+  stringi::stri_length("/output.csv"))
 
 context("Get stan_rdump output path")
 
 expect_match(get_dump_path(), "/dgirt_data.Rdump$")
-expect_true(stringr::str_length(get_dump_path()) >
-  stringr::str_length("/dgirt_data.Rdump"))
+expect_true(stringi::stri_length(get_dump_path()) >
+  stringi::stri_length("/dgirt_data.Rdump"))
