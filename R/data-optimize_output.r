4f530e2e9dcfeaa5a424fe5da100791968d48f77
James Dunham <james@jamesdunham.io>
2015-12-30 12:28:55 -0500

    Updates to data for 0.0.10

---
+++ R/data-optimize_output.r
@@ -0,0 +1,12 @@
+#' Example of dgirt optimization output
+#'
+#' An example of the return value of `dgirt` when `method = "optimize"`.
+#'
+#' @docType data
+#' @name optimize_output
+#' @usage optimize_output
+#' @format A list whose elements correspond to DGIRT parameters and
+#' contain estimates of those parameters.
+#' @examples
+#' optimize_output
+NULL
