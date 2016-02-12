9bb76cefbb8435496726f3f8eef0a8f5cfbdffaa
James Dunham <james@jamesdunham.io>
2016-02-12 09:33:12 -0500

    Stop automated delinting

--- tests/testthat/test-zlint.r
+++
@@ -1,7 +0,0 @@
-# https://github.com/jimhester/lintr#testthat
-if (requireNamespace("lintr", quietly = TRUE)) {
-  testthat::context("lints")
-  testthat::test_that("Package Style", {
-    devtools::lint(".", linters = lintr::with_defaults(line_length_linter = lintr::line_length_linter(110)))
-  })
-}
