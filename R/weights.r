12f9335cd06c5bf3ffe75947a30a6344857a6629
James Dunham <james@jamesdunham.io>
2016-02-11 19:11:36 -0500

    Rename args

--- R/weights.r
+++ R/weights.r
@@ -2,34 +2,30 @@
 #'
 #' Strata in the sampled data must be a subset of those in the population targets.
 #' There can't be missingness in the stratifying variables.
-#' @param .level1 \code{data.frame} with survey responses at the lowest level of aggregation.
-#' @param .arg Arguments to `wrangle`
+#' @param level1 \code{data.frame} with survey responses at the lowest level of aggregation.
+#' @param arg Arguments to `wrangle`
 #' @return \code{data_frame} of survey responses with new weights normalized to have mean 1 in each period.
-create_weights <- function(.level1, .arg) {
+create_weights <- function(level1, arg) {
 
-  weight_formulas <- create_formulas(.arg$target_groups)
-  weight_vars <- get_weight_vars(.arg$target_groups)
+  weight_formulas <- create_formulas(arg$target_groups)
+  weight_vars <- get_weight_vars(arg$target_groups)
 
-  # We can't have missingness in the stratifying variables of the sampled data
-  assertthat::noNA(.level1[, weight_vars])
+  check_levels(level1, arg$targets, weight_vars)
 
-  # All strata in the data must appear in the target matrix. expand.grid
-  # errored out, so do this, though it takes a while
-  .level1 <- .level1 %>%
+  level1 <- level1 %>%
     dplyr::mutate_(stratum = ~interaction(.[, weight_vars], drop = TRUE))
-  .arg$targets <- .arg$targets %>%
+  arg$targets <- arg$targets %>%
     dplyr::ungroup() %>%
     dplyr::mutate_(stratum = ~interaction(.[, weight_vars], drop = TRUE))
-  assertthat::assert_that(is_subset(.level1$stratum, .arg$targets$stratum))
 
   # We'll create a design object from the target data.frame; this is a
   # data.frame with attributes that indicate the survey design
-  target_design <- survey::svydesign(ids = ~1, data = .arg$targets,
-    weights = formula(paste0("~", .arg$target_proportion)))
+  target_design <- survey::svydesign(ids = ~1, data = arg$targets,
+    weights = formula(paste0("~", arg$target_proportion)))
 
-  rake_weight <- function(.level1, formula.list, target_design) {
-    ds <- survey::svydesign(ids = ~1, data = .level1,
-      weights = formula(paste0("~", .arg$survey_weight)))
+  rake_weight <- function(level1, formula.list, target_design) {
+    ds <- survey::svydesign(ids = ~1, data = level1,
+      weights = formula(paste0("~", arg$survey_weight)))
     # NB this weight variable here is the individual weight
     pop.list <- lapply(formula.list, survey::svytable, design = target_design)
     rk <- rake_partial(design = ds, sample.margins = formula.list,
@@ -38,15 +34,15 @@ create_weights <- function(.level1, .arg) {
     return(wts)
   }
 
-  .level1$preweight <- rake_weight(.level1, formula.list = weight_formulas,
+  level1$preweight <- rake_weight(level1, formula.list = weight_formulas,
     target_design = target_design)
 
-  .level1 <- .level1 %>%
-    dplyr::group_by_(.dots = .arg$t_var) %>%
+  level1 <- level1 %>%
+    dplyr::group_by_(.dots = arg$t_var) %>%
     dplyr::mutate_(preweight = ~preweight / mean(preweight, na.rm = TRUE)) %>%
     dplyr::ungroup()
 
-  return(.level1)
+  return(level1)
 }
 
 create_formulas <- function(target_groups) {
@@ -63,4 +59,25 @@ create_formulas <- function(target_groups) {
 
 get_weight_vars <- function(target_groups) {
   unique(unlist(target_groups))
+}
+
+check_levels <- function(level1, targets, weight_vars) {
+  for (s in weight_vars) {
+    # We can't have missingness in the stratifying variables of the sampled data
+    if (!assertthat::noNA(level1[[s]])) {
+      stop("values of '", s, "' in item data include NA")
+    }
+    # All strata in the data must appear in the target matrix. expand.grid
+    # errored out, so do this, though it takes a while
+    class_match <- identical(class(level1[[s]]), class(targets[[s]]))
+    if (!class_match) {
+      stop("class of '", s, "' differs between item data (", class(level1[[s]]), ") and target data (",
+        class(targets[[s]]), ")")
+    }
+    values_match <- is_subset(level1[[s]], targets[[s]])
+    if (!values_match) {
+      stop("values of '", s, "' in item data missing from target data: ", paste(setdiff(level1[[s]], targets[[s]]),
+          collapse = ", "))
+    }
+  }
 }
