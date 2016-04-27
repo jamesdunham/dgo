5ba4ddf34616702bcd45767a1801092707dba9c1
James Dunham <james@jamesdunham.io>
2016-04-27 10:23:48 -0400

    clean and document state target data as `targets`

---
+++ R/data-targets.r
@@ -0,0 +1,16 @@
+#' Yearly U.S. Population Proportions.
+#'
+#' A table giving U.S. population proportions by year for combinations of
+#' demographic variables. Data are from the U.S. Census.
+
+#' When using this table to adjust survey weights via the `target_data`
+#' arguments of `shape`, note that its proportions sum to to 1 within years.
+#'
+#' @docType data
+#' @name targets
+#' @usage targets
+#' @format A `data.frame` with 312,120 rows and 6 columns.
+#' @seealso \url{http://www.census.gov/}
+#' @examples
+#' targets
+NULL
