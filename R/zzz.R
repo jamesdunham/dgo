a96a849d83e241ad7313a755ccbc80bf07586d8f
James Dunham <james@jamesdunham.io>
2016-05-04 17:03:22 -0400

    hack zzz for running devtools::load_all and test; fix format of license in description

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
 .onLoad <- function(libname, pkgname) {
-  loadRcppModules()
+  if (!substr(as.character(sys.call(1L))[1], 1, 8) == "devtools") {
+    loadRcppModules()
+  } else if (!is.loaded("stan_fit42016_04_20_mod")) {
+    loadModule("stan_fit42016_04_20_mod", TRUE)
+    # res <- dyn.load("~/projects/dgirt/src/dgirt.so")
+    # mod <- Rcpp::Module("stan_fit42016_04_20_mod", PACKAGE = res)
+    # source('R/stanmodels.R')
+    # stanmodels <- new(stanmodels)
+  }
 }
