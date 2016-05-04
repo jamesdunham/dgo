7ccef0b4aae26c6108bfc43ff26f3fd0520b8633
James Dunham <james@jamesdunham.io>
2016-05-04 13:54:42 -0400

    rewrite for precompiling models

--- R/import_rcpp.r
+++
@@ -1,3 +0,0 @@
-#' @useDynLib dgirt
-#' @importFrom Rcpp sourceCpp
-NULL
--- tests/testthat/test-dgirtfit.r
+++ tests/testthat/test-dgirtfit.r
@@ -1,14 +1,16 @@
+library(dgirt)
+
 suppressMessages({
   context("dgirtfit class")
 
-  # NOTE: until model is precompiled this test takes too long 
   res <- dgirt(toy_dgirt_in, iter = 10, chains = 1)
   test_that("dgirt returns class dgirtfit", {
     expect_s4_class(res, "dgirtfit")
   })
 
   test_that("dgirt methods work", {
-    expect_output(show(toy_dgirtfit), "Samples were drawn using NUTS")
+    res <- dgirt(toy_dgirt_in, iter = 10, chains = 1)
+    expect_output(show(res), "Samples were drawn using NUTS")
     expect_silent(summary(toy_dgirtfit))
     expect_silent(extract(toy_dgirtfit))
     expect_silent(get_posterior_mean(toy_dgirtfit))
