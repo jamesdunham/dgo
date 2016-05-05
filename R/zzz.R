62ad6bdf03954c6155723d24646b2b92cf61b49d
James Dunham <james@jamesdunham.io>
2016-05-04 22:50:37 -0400

    semi-stable hack for using devtools with Rcpp::loadModules

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,17 @@
 .onLoad <- function(libname, pkgname) {
   if (!substr(as.character(sys.call(1L))[1], 1, 8) == "devtools") {
-    loadRcppModules()
-  } else if (!is.loaded("stan_fit42016_04_20_mod")) {
-    loadModule("stan_fit42016_04_20_mod", TRUE)
-    # res <- dyn.load("~/projects/dgirt/src/dgirt.so")
-    # mod <- Rcpp::Module("stan_fit42016_04_20_mod", PACKAGE = res)
-    # source('R/stanmodels.R')
-    # stanmodels <- new(stanmodels)
+    cat("loadRcppModules!")
+    Rcpp::loadRcppModules()
+    # } else if (!".__C__Rcpp_model_2016_04_20" %in% ls(all.names = TRUE)) {
+    #   Rcpp::loadModule("stan_fit42016_04_20_mod", TRUE)
+    #   # res <- dyn.load("~/projects/dgirt/src/dgirt.so")
+    #   # mod <- Rcpp::Module("stan_fit42016_04_20_mod", PACKAGE = res)
+    #   # source('R/stanmodels.R')
+    #   # stanmodels <- new(stanmodels)
+    # }
   }
+}
+
+.onAttach <- function(libname, pkgname) {
+    Rcpp::loadModule("stan_fit42016_04_20_mod", TRUE)
 }
