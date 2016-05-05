19c73e0d061427989553f82c5866ef0ce2ddddcf
James Dunham <james@jamesdunham.io>
2016-05-05 07:32:38 -0400

    get failure instead of error in test of show method for stan/dgirtfit

--- tests/testthat/test-dgirtfit.r
+++ tests/testthat/test-dgirtfit.r
@@ -10,7 +10,7 @@ suppressMessages({
 
   test_that("dgirt methods work", {
     res <- dgirt(toy_dgirt_in, iter = 10, chains = 1)
-    expect_output(show(res), "Samples were drawn using NUTS")
+    expect_is(tryCatch(show(res), error = function(e) e), "stanfit")
     expect_silent(summary(toy_dgirtfit))
     expect_silent(extract(toy_dgirtfit))
     expect_silent(get_posterior_mean(toy_dgirtfit))
