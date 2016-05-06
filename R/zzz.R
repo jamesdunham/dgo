4052fa63ee8cc975b4411f31b793947d46c309c4
James Dunham <james@jamesdunham.io>
2016-05-06 12:09:44 -0400

    make .onAttach conditional on a devtools call

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,8 @@
   }
 }
 
-# .onAttach <- function(libname, pkgname) {
-#     Rcpp::loadModule("stan_fit42016_04_20_mod", TRUE)
-# }
+if (substr(as.character(sys.call(1L))[1], 1, 8) == "devtools") {
+  .onAttach <- function(libname, pkgname) {
+    Rcpp::loadModule("stan_fit42016_04_20_mod", TRUE)
+  }
+}
