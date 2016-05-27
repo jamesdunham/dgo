5cb94ca73777cfe6c997314a523db57e9b8010b8
James Dunham <james@jamesdunham.io>
2016-05-27 17:48:16 -0400

    Revert refactor of item aggregation
    This reverts commit b2a2aad573d9e605bd2709e2c56f9b4a2c9feb6b.

--- R/dichotomize_item_responses.r
+++ R/dichotomize_item_responses.r
@@ -8,8 +8,8 @@ dichotomize <- function(item_data, ctrl) {
     gt_cols <- dichotomize_cpp(item_data[[i]])
     cn <- paste0(i, sub("^X", "", names(gt_cols)))
     item_data[, c(cn) := gt_cols]
-    data.table::setattr(item_data, "gt_items",
-                        c(attr(item_data, "gt_items"), cn))
+    data.table::setattr(item_data, "gt_items",c(attr(item_data, "gt_items"),
+                                                cn))
   }
   invisible(item_data)
 }
