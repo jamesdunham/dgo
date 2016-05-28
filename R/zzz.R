0ff5c0e36efb7da42f5becd28241b9e95de0bfef
James Dunham <james@jamesdunham.io>
2016-05-28 11:30:13 -0400

    fix onLoad for model modules (h/t rstanarm)

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,4 @@
-# Reload modules on devtools::load_all()
-if (substr(as.character(sys.call(1L))[1], 1, 8) == "devtools") {
-  .onAttach <- function(libname, pkgname) {
-    Rcpp::loadModule("stan_fit42016_04_20_mod", TRUE)
-  }
+.onLoad <- function(libname, pkgname) {
+  modules <- paste0("stan_fit4", names(stanmodels), "_mod")
+  for (m in modules) loadModule(m, what = TRUE)
 }
