1fc3d37e64b1c0d0fab1bea2485d4f6710b3e2cd
James Dunham <james@jdunham.io>
2017-01-25 19:18:53 -0500

    fix to avoid a warning possible when length(modifier_names) > 1

--- R/shape.r
+++ R/shape.r
@@ -260,7 +260,7 @@ shape_hierarchical_data <- function(item_data, modifier_data, d_in, ctrl, t1) {
     modifier_names <- NA
   }
 
-  if (!length(modifier_data) | is.na(modifier_names)) {
+  if (!length(modifier_data) | any(is.na(modifier_names))) {
       zz.names <- list(ctrl@time_filter, dimnames(d_in$XX)[[2]], "")
       zz <- array(data = 0, dim = lapply(zz.names, length), dimnames = zz.names)
     } else {
