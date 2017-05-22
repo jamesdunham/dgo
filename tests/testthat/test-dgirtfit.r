4f742cdb7a1aa513a7f3019e8b80307a1cb6f809
James Dunham <james@jdunham.io>
2017-05-22 09:11:48 -0400

    clean up .Rbuildignore

--- tests/testthat/test-dgirtfit.r
+++ tests/testthat/test-dgirtfit.r
@@ -3,10 +3,12 @@ context("dgirtfit class")
 suppressWarnings({
   data(toy_dgirt_in)
   data(opinion)
-  dgirt_ret <- dgirt(toy_dgirt_in, iter = 5, chains = 1, seed = 42)
+  test_iter <- 5
+  test_chains <- 1
+  dgirt_ret <- dgirt(toy_dgirt_in, iter = test_iter, chains = test_chains, seed = 42)
   dgmrp_in <- shape(opinion, item_names = "abortion", time_name = "year",
     geo_name = "state", group_names = "race3", geo_filter = c("CA", "GA"))
-  dgmrp_ret <- dgmrp(dgmrp_in, iter = 5, chains = 1, seed = 42)
+  dgmrp_ret <- dgmrp(dgmrp_in, iter = test_iter, chains = test_chains, seed = 42)
 })
 
 test_that("dgirt returns class dgirt_fit", {
@@ -22,6 +24,17 @@ test_that("dgmrp returns class dgmrp_fit/dgo_fit/stanfit", {
 })
 
 for (ret in c(dgirt_ret, dgmrp_ret)) {
+
+  test_that("further arguments pass to rstan via ...", {
+    expect_equal(length(ret@stan_args), test_chains)
+    expect_true(all(sapply(toy_dgirtfit@stan_args, function(x) x$iter) ==
+        test_iter))
+  })
+
+  test_that("defaults pass to rstan", {
+    expect_true(all(sapply(toy_dgirtfit@stan_args, function(x) x$init_r) ==
+        1L))
+  })
 
   test_that("dgirt methods work", {
     expect_output(print(ret), "dgirt samples from")
